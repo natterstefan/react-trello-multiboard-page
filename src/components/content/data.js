ac57069baa83444acb9b6d30ad5e62ecb49b7244
natterstefan <stefan@natter.at>
2018-07-01 22:52:52 +0200

    fix image url

--- src/components/content/data.js
+++ src/components/content/data.js
@@ -1,7 +1,7 @@
 const cardData = [
   {
     img:
-      'https://res.cloudinary.com/dgqdvqyz3/image/upload/v1530115641/Screen_Shot_on_2018-06-27_at_17_3A52_3A06_e9pdue.png',
+      'https://res.cloudinary.com/dgqdvqyz3/image/upload/v1530115641/react-trello-multiboard/Screen_Shot_on_2018-06-27_at_17_3A52_3A06_e9pdue.png',
     title: 'Source: GitHub',
     link: 'https://github.com/natterstefan/react-trello-multiboard',
     cols: 2,
@@ -9,13 +9,13 @@ const cardData = [
   },
   {
     img:
-      'https://res.cloudinary.com/dgqdvqyz3/image/upload/v1530115668/oWswTtR4_400x400_1_m6wtld.jpg',
+      'https://res.cloudinary.com/dgqdvqyz3/image/upload/v1530115668/react-trello-multiboard/oWswTtR4_400x400_1_m6wtld.jpg',
     title: 'Author: natterstefan',
     link: 'https://twitter.com/natterstefan'
   },
   {
     img:
-      'https://res.cloudinary.com/dgqdvqyz3/image/upload/v1530115666/trello_multiboard_example_u6in6u.gif',
+      'https://res.cloudinary.com/dgqdvqyz3/image/upload/v1530115666/react-trello-multiboard/trello_multiboard_example_u6in6u.gif',
     title: 'Example',
     link: 'https://multiboardfortrello.com/example'
   }
