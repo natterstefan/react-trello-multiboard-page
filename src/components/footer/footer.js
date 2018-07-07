0efc5a8a08733a104b5c51bbeaff6f16cdff27e7
natterstefan <stefan@natter.at>
2018-07-07 08:15:29 +0200

    fix footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
