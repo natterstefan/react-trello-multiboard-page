1ad41358cb6f32483c8264857e927630d23839b4
Stefan Natter <stefan@natter.at>
2022-08-26 20:43:25 +0200

    fix: removed unused config.js

--- config.example.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  googleTagManagerId: 123456789,
-  googleAnalytics: 123456789
-}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,3 @@
-const config = require('./config')
-
 module.exports = {
   siteMetadata: {
     title: 'React-Trello-Multiboard',
